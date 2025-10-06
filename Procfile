0927665153b0ef6fa3e2ddfbdf228817a318fb7f
fathanjamilrises <fathan.jamil2008@smkn4-tsm.sch.id>
2025-10-06 17:12:31 +0700

    bismillah fix

--- Procfile
+++ Procfile
@@ -1,1 +1,2 @@
 web: gunicorn smkn4django.wsgi
+
--- smkn4django/settings.py
+++ smkn4django/settings.py
@@ -11,6 +11,7 @@ https://docs.djangoproject.com/en/5.2/ref/settings/
 """
 
 from pathlib import Path
+import os
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -25,7 +26,7 @@ SECRET_KEY = 'django-insecure-!$(w*^1blv@*5^880ng0h67ap7hdce_*ciukj@)m+nzw4y22dv
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['*']
 
 
 # Application definition
@@ -115,8 +116,8 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/5.2/howto/static-files/
 
-STATIC_URL = 'static/'
-STATIC_ROOT = BASE_DIR / 'staticfiles'
+STATIC_URL = '/static/'
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.2/ref/settings/#default-auto-field
